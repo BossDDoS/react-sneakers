a81c228344bcb9391054e49d1c9e8dc652a3c40f
Maxim L <maksim.ldokov.97@mail.ru>
2022-12-12 21:14:19 +0400

    Added map components. Tested some modules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+let arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12998,
+    imageUrl: "img/sneakers/1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "img/sneakers/2.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "img/sneakers/3.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -16,13 +34,14 @@ function App() {
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((sneaker) => (
+            <Card
+              title={sneaker.title}
+              price={sneaker.price}
+              imageUrl={sneaker.imageUrl}
+              onClick={() => console.log(sneaker)}
+            />
+          ))}
         </div>
       </div>
     </div>
